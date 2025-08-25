dde1f735c27cf7da752e3b34e66d798441a338d9
omegaalfa <omgaalfa.wr3@gmail.com>
2025-08-24 21:30:06 -0300

    chore: Refactoring code

--- src/PromiseInterface.php
+++ src/PromiseInterface.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace Omegaalfa\Promise;
 
@@ -12,17 +12,17 @@ interface PromiseInterface
      */
     public function then(callable $onFulfilled): PromiseInterface;
 
-	/**
-	 * @param  callable  $onRejected
-	 *
-	 * @return PromiseInterface
-	 */
+    /**
+     * @param callable $onRejected
+     *
+     * @return PromiseInterface
+     */
     public function catch(callable $onRejected): PromiseInterface;
 
-	/**
-	 * @param  callable  $onFinally
-	 *
-	 * @return PromiseInterface
-	 */
-	public function finally(callable $onFinally): PromiseInterface;
+    /**
+     * @param callable $onFinally
+     *
+     * @return PromiseInterface
+     */
+    public function finally(callable $onFinally): PromiseInterface;
 }
